97f88c25e3ce1a0d609af499ef2d6b27e10fc774
Rafael Noronha <rafaelsnoronha@hotmail.com>
2023-02-24 14:10:19 -0300

    reiniciar, iniciar

--- js/app.js
+++ js/app.js
@@ -4,9 +4,24 @@ createApp( {
     data() {
         return {
             status: 'start', // start | started | finished
-            vidaJogador: 100,
-            vidaMonstro: 100,
+            vidaJogador: 50,
+            vidaMonstro: 80,
             logs: []
+        }
+    },
+    methods: {
+        reset() {
+            this.vidaJogador = 100
+            this.vidaMonstro = 100
+            this.logs = []
+        },
+        iniciar() {
+            this.status = 'started'
+            this.reset()
+        },
+        reiniciar() {
+            this.status = 'start'
+            this.reset()
         }
     }
 } ).mount( '#app' )
