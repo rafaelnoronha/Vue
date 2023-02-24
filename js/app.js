70f681af6b7e93606e693da10f0eaf0742bf84c2
˜Rafael <rafaelsnoronha@hotmail.com>
2023-02-23 23:04:43 -0200

    Avanço no desafio

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ const { createApp } = Vue
 createApp( {
     data() {
         return {
-            mensagem: 'Olá mundo!!'
+            status: 'start', // start | started | finished
+            vidaJogador: 100,
+            vidaMonstro: 100,
+            logs: []
         }
     }
 } ).mount( '#app' )
